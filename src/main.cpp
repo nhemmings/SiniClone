a61af5333b7932d30abdc2a0a1840de5b436d9d3
nhemmings <natehemm@gmail.com>
2015-08-12 23:32:32 -0500

    Added frame timing to the main game loop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,42 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 #include "EntityManager.hpp"
+
+void runServerFrame(const sf::Time & dt);
+void runClientFrame(const sf::Time & dt, sf::RenderWindow & rwindow);
+
+class dtAverage {
+private:
+    static const unsigned short dtHistorySize = 10;
+    sf::Time dtSMA;
+    sf::Time dtHistory[dtHistorySize];
+    unsigned short idx;
+public:
+    dtAverage(const sf::Time & dtIdeal);
+    sf::Time update(sf::Time & dtCurrent);
+};
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    using sf::Clock;
+    using sf::Time;
+
+    static const Time s_tServerFrame = sf::seconds(1.0f/20.0f);
+    static const Time s_dtIdeal = sf::seconds(1.0f/30.0f);
+    sf::RenderWindow window(sf::VideoMode(500, 300), "SFML works!");
     EntityManager gameEntityManager(window);
+
+    /* Initialize clocks */
+    Time dtReal = s_dtIdeal;        // The real frame delta time, initialized to ideal value
+    dtAverage m_dtAvg(s_dtIdeal);
+    Time dtServer = sf::Time::Zero; // Delta time since last server frame
+    Clock realTimeClock;
+    Time tBegin = realTimeClock.getElapsedTime();
 
     // MAIN GAME LOOP
     while (window.isOpen())
     {
+        // System message pump
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -16,9 +44,44 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.display();
+        // Server update
+        dtServer += dtReal;
+        if (dtServer >= s_tServerFrame) {
+            runServerFrame(dtServer);
+            dtServer -= s_tServerFrame;
+        }
+
+        // Client update
+        runClientFrame(dtReal, window);
+
+        // Timer update
+        Time tEnd = realTimeClock.getElapsedTime();
+        Time dtThisFrame = tEnd - tBegin;
+        dtReal = m_dtAvg.update(dtThisFrame);
+        tBegin = tEnd;
     }
 
     return 0;
+}
+
+void runServerFrame(const sf::Time & dt) {
+
+}
+
+void runClientFrame(const sf::Time & dt, sf::RenderWindow & rwindow) {
+    rwindow.clear();
+    rwindow.display();
+}
+
+dtAverage::dtAverage(const sf::Time & dtIdeal) : dtSMA(dtIdeal), idx(0) {
+    for (int i = 0; i < dtHistorySize; i++) {
+        dtHistory[i] = dtIdeal;
+    }
+}
+
+sf::Time dtAverage::update(sf::Time & dtCurrent) {
+    dtSMA = dtSMA + dtCurrent/float(dtHistorySize) - dtHistory[idx]/float(dtHistorySize);
+    dtHistory[idx] = dtCurrent;
+    idx = (idx + 1) % dtHistorySize;
+    return dtSMA;
 }
