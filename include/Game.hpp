550b694589bb11601af34fc6572bfe609d04d998
nhemmings <natehemm@gmail.com>
2016-03-28 16:31:49 -0500

    Revisited the project with fresh perspective and made some changes. I created a new project to mess around with the basics of bridging the two coordinate systems and ended up realizing my head was up my ass. Didn't need the CoordAdapter class at all. I have new (hopefully better) ideas for the entity objects and object pools, so I'm removing those from the current project and I will be reusing pieces of the code in the new concepts.

--- include/Game.hpp
+++ include/Game.hpp
@@ -2,12 +2,8 @@
 #define GAME_HPP
 
 #include <SFML/Graphics.hpp>
-#include "CoordAdapter2D.hpp"
-#include "AsteroidPool.hpp"
-
-#ifdef DEBUG
-#include <fstream>
-#endif // DEBUG
+#include "particle.hpp"
+#include "Vector2D.hpp"
 
 /**
  *  The primary game logic and entry point. Initializes the game's subsystems and manages the viewing window.
@@ -17,52 +13,34 @@ class Game
 private:
     enum GameType {SinglePlayer, MultiPlayer}; /**< Enumeration of game modes. */
 
-    /**
-     *  Provides helper functions to convert between the SFML coordinate system and the physics engine's coordinate
-     *  system. SFML puts the origin in the top left corner of the window and the Y-axis increases down. The physics
-     *  engine puts the origin in the lower left and the Y-axis increases upward, which is more intuitive for physics
-     *  simulation (e.g. gravity).
-     */
-    class SFMLCoordAdapter : public CoordAdapter2D {
-    public:
-        SFMLCoordAdapter(const Vector2u windowSize) : CoordAdapter2D(windowSize) {}
-        float physToWindowX(const float x) const;
-        float physToWindowY(const float y) const;
-        Vector2u physToWindow(const Vector2D& vec) const;
-
-        float windowToPhysX(const float x) const;
-        float windowToPhysY(const float y) const;
-        Vector2D windowToPhys(const Vector2u& vec) const;
-    };
-
     GameType m_GameType;
     sf::RenderWindow m_window;
     sf::View m_mainView;
     const sf::Time m_dtServerFrame;
     const sf::Time m_dtIdeal;
-    SFMLCoordAdapter m_CoordAdapter;
-    AsteroidPool m_AsteroidPool;
     bool m_isRunning;
 
     /**< Data for tracking a Simple Moving Average of frame delta times */
-    static const unsigned short m_dtSMA_period = 10;
+    static const sf::Uint8 m_dtSMA_period = 10;
     sf::Time m_dtSMA;
     sf::Time m_dtHistory[m_dtSMA_period];
-
-    #ifdef DEBUG
-    std::ofstream m_outFile;
-    #endif // DEBUG
 
     void startGame(const GameType newGameType);
     void runServerFrame(const sf::Time & dt);
     void runClientFrame(const sf::Time & dt, sf::RenderWindow & rwindow);
+
     void dtSMAinit(const sf::Time & dtIdeal);
     sf::Time dtSMAupdate(const sf::Time & dtCurrent);
+
+    sf::CircleShape* circles;
+    physics::Particle* particles;
+
+    sf::Vector2f coordWorldToView(const physics::Vector2D coord) const;
+    physics::Vector2D coordViewToWorld(const sf::View & view, const sf::Vector2f & coord);
 public:
     Game();
     virtual ~Game();
 
     int run();
 };
-
 #endif // GAME_HPP
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,27 +1,23 @@
 #include <cstdlib>
 #include "Game.hpp"
+#include <ctime>
 
-#ifdef DEBUG
-#include <iostream>
-#endif // DEBUG
-
-Game::Game() : m_dtServerFrame(sf::seconds(1.0/20.0f)), m_dtIdeal(sf::seconds(1.0/1500.0f)),
-               m_CoordAdapter(Vector2u(800,600)), m_AsteroidPool(m_CoordAdapter, 25), m_isRunning(false)
+Game::Game() : m_mainView(sf::Vector2f(0, 0), sf::Vector2f(400, 400)), m_dtServerFrame(sf::seconds(1.0/20.0f)),
+               m_dtIdeal(sf::seconds(1.0/60.0f)), m_isRunning(false)
 {
     sf::ContextSettings settings;
     settings.antialiasingLevel = 6;
-    m_window.create(sf::VideoMode(800, 600), "Siniclone", sf::Style::Default, settings);
+    m_window.create(sf::VideoMode(800, 800), "Siniclone", sf::Style::Default, settings);
+    m_window.setView(m_mainView);
     dtSMAinit(m_dtIdeal);
     m_dtSMA = m_dtIdeal;
 
-    #ifdef DEBUG
-    m_outFile.open("dtSMA.csv");
-    #endif // DEBUG
+    circles = new sf::CircleShape[40];
+    particles = new physics::Particle[40];
 }
 
 Game::~Game()
 {
-    m_outFile.close();
 }
 
 int Game::run() {
@@ -51,9 +47,22 @@ int Game::run() {
             else if (event.type == sf::Event::KeyPressed) {
                 if (event.key.code == sf::Keyboard::Escape)
                     m_window.close();
-                else if (event.key.code == sf::Keyboard::S && !m_isRunning) {
+                else if (event.key.code == sf::Keyboard::W) {
+                    m_mainView.move(0, -1);
+                }
+                else if (event.key.code == sf::Keyboard::A) {
+                    m_mainView.move(-1, 0);
+                }
+                else if (m_isRunning && event.key.code == sf::Keyboard::S) {
+                    m_mainView.move(0, 1);
+                }
+                else if (event.key.code == sf::Keyboard::D) {
+                    m_mainView.move(1, 0);
+                }
+                else if (!m_isRunning && event.key.code == sf::Keyboard::S) {
                     startGame(SinglePlayer);
-                } else if (event.key.code == sf::Keyboard::M && !m_isRunning) {
+                }
+                else if (!m_isRunning && event.key.code == sf::Keyboard::M) {
                     startGame(MultiPlayer);
                 }
             }
@@ -83,8 +92,13 @@ void Game::runServerFrame(const sf::Time & dt) {
 
 void Game::runClientFrame(const sf::Time & dt, sf::RenderWindow & rwindow) {
     rwindow.clear();
-    m_AsteroidPool.updateAsteroids(dt);
-    m_AsteroidPool.renderAsteroids(rwindow);
+    rwindow.setView(m_mainView);
+
+    for (int i = 0; i < 40; i++) {
+        particles[i].integrate(dt.asSeconds());
+        circles[i].setPosition(coordWorldToView((particles[i].getPosition())));
+        rwindow.draw(circles[i]);
+    }
     rwindow.display();
 }
 
@@ -98,52 +112,35 @@ void Game::dtSMAinit(const sf::Time & dtIdeal) {
 sf::Time Game::dtSMAupdate(const sf::Time & dtCurrent) {
     static unsigned short idx = 0;
 
-    #ifdef DEBUG
-    m_outFile << dtCurrent.asSeconds() << ",";
-    #endif // DEBUG
-
     sf::Time dtCurrentScaled = dtCurrent / sf::Int64(m_dtSMA_period);
     m_dtSMA = m_dtSMA - m_dtHistory[idx] + dtCurrentScaled;
     m_dtHistory[idx] = dtCurrentScaled;
     idx = (idx + 1) % m_dtSMA_period;
 
-    #ifdef DEBUG
-    m_outFile << m_dtSMA.asSeconds() << std::endl;
-    #endif // DEBUG
-
     return m_dtSMA;
 }
 
 void Game::startGame(const GameType newGameType) {
-    m_GameType = newGameType;
-    m_AsteroidPool.addAsteroid(10, 40, 10, 0.99, Vector2D(20, 20), Vector2D(40.0, 100.0), Vector2D(0.0, -10.0));
-    m_AsteroidPool.addAsteroid(10, 40, 10, 0.99, Vector2D(780, 20), Vector2D(-40.0, 100.0), Vector2D(0.0, -10.0));
+    srand(time(NULL));
+    for (int i = 0; i < 40; i++) {
+        circles[i].setRadius(rand() % 10 + 5);
+        circles[i].setPointCount(20);
+        circles[i].setFillColor(sf::Color(128, 128, 128));
+        circles[i].setOutlineColor(sf::Color::White);
+        circles[i].setOutlineThickness(-2.0);
+
+        particles[i].setPosition(rand() % 800 - 400, rand() % 800 - 400);
+        particles[i].setVelocity(rand() % 60 - 30, rand() % 60 - 30);
+        particles[i].setMass(0.01);
+        particles[i].setDamping(0.999);
+    }
     m_isRunning = true;
 }
 
-//**********************************************************************************************************************
-//  SFMLCoordAdapter Methods
-//**********************************************************************************************************************
-inline float Game::SFMLCoordAdapter::physToWindowX(const float x) const {
-    return x;
+sf::Vector2f Game::coordWorldToView(const physics::Vector2D coord) const {
+    return sf::Vector2f(coord.x, -coord.y);
 }
 
-inline float Game::SFMLCoordAdapter::physToWindowY(const float y) const {
-    return (m_WindowSize.y - y);
-}
-
-inline Vector2u Game::SFMLCoordAdapter::physToWindow(const Vector2D& vec) const {
-    return Vector2u(vec.x, m_WindowSize.y - vec.y);
-}
-
-inline float Game::SFMLCoordAdapter::windowToPhysX(const float x) const {
-    return x;
-}
-
-inline float Game::SFMLCoordAdapter::windowToPhysY(const float y) const {
-    return (m_WindowSize.y - y);
-}
-
-inline Vector2D Game::SFMLCoordAdapter::windowToPhys(const Vector2u& vec) const {
-    return Vector2D(vec.x, vec.y);
+physics::Vector2D Game::coordViewToWorld(const sf::View & view, const sf::Vector2f & coord) {
+    return physics::Vector2D();
 }
